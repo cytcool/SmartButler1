31e149ee43a8ab938757c7569bebb9114836fbf8
cytcool <675149948@qq.com>
2017-12-09 11:20:30 +0800

    WebView网页详情处理

--- app/src/main/java/com/atguigu/smartbutler1/fragment/WechatFragment.java
+++ app/src/main/java/com/atguigu/smartbutler1/fragment/WechatFragment.java
@@ -1,16 +1,19 @@
 package com.atguigu.smartbutler1.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.atguigu.smartbutler1.R;
 import com.atguigu.smartbutler1.adapter.WeChatAdapter;
 import com.atguigu.smartbutler1.entity.CourierData;
 import com.atguigu.smartbutler1.entity.WeChatData;
+import com.atguigu.smartbutler1.ui.WebViewActivity;
 import com.atguigu.smartbutler1.utils.StaticClass;
 import com.kymjs.rxvolley.RxVolley;
 import com.kymjs.rxvolley.client.HttpCallback;
@@ -45,7 +48,15 @@ public class WechatFragment extends Fragment {
 
     private void findView(View view) {
         mListView = (ListView) view.findViewById(R.id.mListView);
-
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra("title",mListTitle.get(i));
+                intent.putExtra("url",mListUrl.get(i));
+                startActivity(intent);
+            }
+        });
         //解析接口
         String url = "http://v.juhe.cn/weixin/query?key=" + StaticClass.WECHAT_KEY + "&ps=100";
         RxVolley.get(url, new HttpCallback() {
@@ -64,10 +75,17 @@ public class WechatFragment extends Fragment {
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject json = (JSONObject) jsonArray.get(i);
                 WeChatData data = new WeChatData();
-                data.setTitle(json.getString("title"));
+
+                String title = json.getString("title");
+                String url = json.getString("url");
+
+                data.setTitle(title);
                 data.setSource(json.getString("source"));
                 data.setImgurl(json.getString("firstImg"));
                 mList.add(data);
+
+                mListTitle.add(title);
+                mListUrl.add(url);
             }
             WeChatAdapter adapter = new WeChatAdapter(getActivity(), mList);
             mListView.setAdapter(adapter);
---
+++ app/src/main/java/com/atguigu/smartbutler1/ui/WebViewActivity.java
@@ -0,0 +1,81 @@
+package com.atguigu.smartbutler1.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import com.atguigu.smartbutler1.R;
+
+/**
+ * Created by CYT on 2017/12/9.
+ */
+
+public class WebViewActivity extends BaseActivity {
+
+    private ProgressBar mProgressBar;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        initView();
+    }
+
+    private void initView() {
+
+        mProgressBar = (ProgressBar) findViewById(R.id.mProgressBar);
+        mWebView = (WebView) findViewById(R.id.mWebView);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        final String url = intent.getStringExtra("url");
+
+        //设置标题
+        getSupportActionBar().setTitle(title);
+
+        //进行加载网页的逻辑
+
+        //支持JS
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //支持缩放
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        //接口回调
+        mWebView.setWebChromeClient(new WebViewClient());
+        //加载网页
+        mWebView.loadUrl(url);
+
+        //本地显示
+        mWebView.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(url);
+                //我接受这个事件
+                return true;
+            }
+        });
+    }
+
+    public class WebViewClient extends WebChromeClient {
+
+        //进度变化的监听
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgressBar.setVisibility(View.GONE);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+
+
+
+}
+
