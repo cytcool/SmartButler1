35cdd6c45a39e6ff3b0c15a27575bd3bac2cb1f7
cytcool <675149948@qq.com>
2017-12-18 21:38:21 +0800

    短信提醒--自定义短信弹框提示

--- app/src/main/java/com/atguigu/smartbutler1/service/SmsService.java
+++ app/src/main/java/com/atguigu/smartbutler1/service/SmsService.java
@@ -5,10 +5,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.telephony.SmsMessage;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.atguigu.smartbutler1.R;
 import com.atguigu.smartbutler1.ui.L;
 import com.atguigu.smartbutler1.utils.StaticClass;
 
@@ -16,14 +23,23 @@ import com.atguigu.smartbutler1.utils.StaticClass;
  * Created by CYT on 2017/12/14.
  */
 
-public class SmsService extends Service {
+public class SmsService extends Service implements View.OnClickListener {
 
     private SmsRecevier smsRecevier;
     //发件人号码
     private String smsPhone;
     //短信内容
     private String smsContent;
+    //窗口管理
+    private WindowManager wm;
+    //布局参数
+    private WindowManager.LayoutParams layoutParams;
+    //添加布局
+    private View mView;
 
+    private TextView tv_phone;
+    private TextView tv_content;
+    private Button btn_send_sms;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -56,6 +72,28 @@ public class SmsService extends Service {
         unregisterReceiver(smsRecevier);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_send_sms:
+                sendSms();
+                //消失窗口
+                if (mView.getParent() != null){
+                    wm.removeView(mView);
+                }
+                break;
+        }
+    }
+
+    private void sendSms() {
+        Uri uri = Uri.parse("smsto:" + smsPhone);
+        Intent intent = new Intent(Intent.ACTION_SENDTO,uri);
+        //设置启动模式
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("sms_body","");
+        startActivity(intent);
+    }
+
 
     //短信广播
     public class SmsRecevier extends BroadcastReceiver{
@@ -73,8 +111,38 @@ public class SmsService extends Service {
                         smsPhone = sms.getOriginatingAddress();
                         //内容
                         smsContent = sms.getMessageBody();
+
+                        //窗口提示
+                        showWindow();
                     }
                 }
         }
+    }
+
+    //窗口提示
+    private void showWindow() {
+        //获取系统服务
+        wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        //获取布局参数
+        layoutParams = new WindowManager.LayoutParams();
+        //定义宽高
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+        //定义标记
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+        //定义格式
+        layoutParams.format = PixelFormat.TRANSLUCENT;
+        //定义类型
+        layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        //加载布局
+        mView = View.inflate(getApplicationContext(), R.layout.sms_item,null);
+        tv_phone = (TextView) mView.findViewById(R.id.tv_phone);
+        tv_content = (TextView) mView.findViewById(R.id.tv_content);
+        btn_send_sms = (Button) mView.findViewById(R.id.btn_send_sms);
+        btn_send_sms.setOnClickListener(this);
+
+
+        //添加View到窗口
+        wm.addView(mView,layoutParams);
     }
 }
