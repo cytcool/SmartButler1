c357dcc294c8bcad575088d558170e957aabdcc3
cytcool <675149948@qq.com>
2018-03-12 22:33:44 +0800

    完善美女社区

--- app/src/main/java/com/atguigu/smartbutler1/adapter/GrilAdapter.java
+++ app/src/main/java/com/atguigu/smartbutler1/adapter/GrilAdapter.java
@@ -1,7 +1,6 @@
 package com.atguigu.smartbutler1.adapter;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,30 +14,24 @@ import com.atguigu.smartbutler1.utils.PicassoUtils;
 
 import java.util.List;
 
-/**
- * Created by CYT on 2017/12/10.
- */
 
 public class GrilAdapter extends BaseAdapter {
 
     private Context mContext;
     private List<GrilData> mList;
-    private LayoutInflater mLayoutInflater;
+    private LayoutInflater inflater;
     private GrilData data;
-
-    private int width,height;
     private WindowManager wm;
+    //屏幕宽
+    private int width;
 
-    public GrilAdapter(Context mContext,List<GrilData> mList){
+    public GrilAdapter(Context mContext, List<GrilData> mList) {
         this.mContext = mContext;
         this.mList = mList;
-        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         width = wm.getDefaultDisplay().getWidth();
-        height = wm.getDefaultDisplay().getHeight();
     }
-
 
     @Override
     public int getCount() {
@@ -46,38 +39,38 @@ public class GrilAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int i) {
-        return mList.get(i);
+    public Object getItem(int position) {
+        return mList.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return i;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder = null;
-        if (view == null){
+        if(convertView == null){
             viewHolder = new ViewHolder();
-            view = mLayoutInflater.inflate(R.layout.gril_item,null);
-            viewHolder.imageView = (ImageView) view.findViewById(R.id.imageview);
-            view.setTag(viewHolder);
+            convertView = inflater.inflate(R.layout.gril_item,null);
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.imageview);
+            convertView.setTag(viewHolder);
         }else {
-            viewHolder = (ViewHolder) view.getTag();
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        //加载数据
-        data = mList.get(i);
-        //加载图片
-        if(!TextUtils.isEmpty(data.getImgUrl())){
-            //加载图片
-            PicassoUtils.loadImageViewSize(mContext, data.getImgUrl(), width/2, 500, viewHolder.imageView);
-        }
-        return view;
+        data = mList.get(position);
+        //解析图片
+        String url = data.getImgUrl();
+
+        PicassoUtils.loadImageViewSize(mContext,url,width/2,500,viewHolder.imageView);
+
+        return convertView;
     }
+
     class ViewHolder{
         private ImageView imageView;
     }
-
 }
+
--- app/src/main/java/com/atguigu/smartbutler1/fragment/GirlsFragment.java
+++ app/src/main/java/com/atguigu/smartbutler1/fragment/GirlsFragment.java
@@ -14,75 +14,88 @@ import android.widget.LinearLayout;
 import com.atguigu.smartbutler1.R;
 import com.atguigu.smartbutler1.adapter.GrilAdapter;
 import com.atguigu.smartbutler1.entity.GrilData;
+
+import com.atguigu.smartbutler1.ui.L;
 import com.atguigu.smartbutler1.utils.PicassoUtils;
-import com.atguigu.smartbutler1.utils.StaticClass;
 import com.atguigu.smartbutler1.view.CustomDialog;
 import com.kymjs.rxvolley.RxVolley;
 import com.kymjs.rxvolley.client.HttpCallback;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
-/**
- * Created by 陈雨田 on 2017/8/29.
- */
+public class GirlsFragment extends Fragment {
 
-public class GirlsFragment extends Fragment{
-
+    //列表
     private GridView mGridView;
+    //数据
     private List<GrilData> mList = new ArrayList<>();
-    private GrilData data;
-
+    //适配器
     private GrilAdapter mAdapter;
-
     //提示框
     private CustomDialog dialog;
     //预览图片
     private ImageView iv_img;
-    //图片地址
+    //图片地址的数据
     private List<String> mListUrl = new ArrayList<>();
+    //PhotoView
     private PhotoViewAttacher mAttacher;
 
+    /**
+     * 1.监听点击事件
+     * 2.提示框
+     * 3.加载图片
+     * 4.PhotoView
+     */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.girls_fragmeng,null);
+        View view = inflater.inflate(R.layout.girls_fragmeng, null);
         findView(view);
         return view;
     }
 
+    //初始化View
     private void findView(View view) {
+
         mGridView = (GridView) view.findViewById(R.id.mGridView);
 
-        //初始化dialog
+        //初始化提示框
         dialog = new CustomDialog(getActivity(), LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT, R.layout.dialog_girl,
                 R.style.Theme_dialog, Gravity.CENTER,R.style.pop_anim_style);
         iv_img = (ImageView) dialog.findViewById(R.id.iv_img);
 
-
-        //数据解析
-        RxVolley.get(StaticClass.GIRL_URL, new HttpCallback() {
+        String welfare = null;
+        try {
+            //Gank升級 需要转码
+            welfare = URLEncoder.encode(getString(R.string.text_welfare), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        //解析
+        RxVolley.get("http://gank.io/api/search/query/listview/category/"+welfare+"/count/50/page/1", new HttpCallback() {
             @Override
             public void onSuccess(String t) {
-                //解析json
-                parsonJson(t);
+                L.i("Girl Json:" + t);
+                parsingJson(t);
             }
         });
 
+        //监听点击事件
         mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //图片加载
-                PicassoUtils.loadImageViewCrop(getContext(),mListUrl.get(i),iv_img);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //解析图片
+                PicassoUtils.loadImaheView(getActivity(), mListUrl.get(position), iv_img);
                 //缩放
                 mAttacher = new PhotoViewAttacher(iv_img);
                 //刷新
@@ -92,21 +105,23 @@ public class GirlsFragment extends Fragment{
         });
     }
 
-    private void parsonJson(String t) {
+    //解析Json
+    private void parsingJson(String t) {
         try {
             JSONObject jsonObject = new JSONObject(t);
             JSONArray jsonArray = jsonObject.getJSONArray("results");
-            for (int i = 0; i < jsonArray.length() ; i++) {
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject json = (JSONObject) jsonArray.get(i);
                 String url = json.getString("url");
                 mListUrl.add(url);
+
+                GrilData data = new GrilData();
                 data.setImgUrl(url);
                 mList.add(data);
             }
+            mAdapter = new GrilAdapter(getActivity(), mList);
             //设置适配器
-            mAdapter = new GrilAdapter(getActivity(),mList);
             mGridView.setAdapter(mAdapter);
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
